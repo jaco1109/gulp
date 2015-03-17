0fd6cb666b35d491ca70795bcb9f80cba5152e87
相田義仁 <aida@i-tank.jp>
2015-03-17 19:25:26 +0900

    いろいろ

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,46 @@
 var gulp = require('gulp');
 
+$ = require('gulp-load-plugins')();
 //　ここで使いたい機能をrequireする
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var styledocco = require('gulp-styledocco');
-var uglify = require('gulp-uglify');
-var plumber = require('gulp-plumber');
-var browser = require('browser-sync');
 
-// 開発用サーバーの立ち上げ
-gulp.task('server', function() {
-  browser({
-    server: {
-      baseDir: './'
-    }
-  });
+gulp.task('svg-min', function() {
+  gulp.src(['./icons/*.svg'])
+      .pipe($.svgmin())
+      .pipe(gulp.dest('./icons'));
 });
+
+gulp.task('fonts', ['svg-min'], function() {
+  gulp.src(['./fonts'])
+      .pipe($.clean());
+
+  gulp.src(['./icons/*.svg'])
+      .pipe($.plumber($.util.log))
+      .pipe($.iconfontCss({
+        fontName: 'icon-font',
+        path: './sass/templates/_icons.scss',
+        targetPath: '../sass/_icons.scss',
+        fontPath: "./fonts/"
+      }))
+      .pipe($.iconfont({
+        fontName: 'icon-font',
+        appendCodepoints: false
+      }))
+      .pipe(gulp.dest('./fonts'));
+})
 
 // sass
 gulp.task('sass', function() {
   gulp.src('sass/**/*.scss') // コンパイル対象となる.scssファイルの場所
-      .pipe(plumber()) // エラーが出てもwatchを止めない
-      .pipe(sass()) // sassをコンパイル
-      .pipe(autoprefixer()) // ベンダープレフィックスを入れる
-      .pipe(gulp.dest('./css')) // コンパイル後のcssの出力先
-      .pipe(browser.reload({stream:true})); // 開発用サーバー内で立ち上げたブラウザ自動でリロードする
+      .pipe($.plumber()) // エラーが出てもwatchを止めない
+      .pipe($.sass()) // sassをコンパイル
+      .pipe($.autoprefixer()) // ベンダープレフィックスを入れる
+      .pipe(gulp.dest('./css')); // コンパイル後のcssの出力先
 });
 
 // styledocco
 gulp.task('styledocco', function() {
   gulp.src('./sass/**/docco.scss') // スタイルガイドを生成する対象となる.scssファイルの場所
-    .pipe(styledocco({
+    .pipe($.styledocco({
       out: 'docs',
       name: 'StyleGuide',
       'no-minify': false
@@ -40,8 +50,8 @@ gulp.task('styledocco', function() {
 // js
 gulp.task('js', function() {
   gulp.src(['js/**/*.js', '!js/min/**/*.js']) // 圧縮対象となる.jsファイルの場所(圧縮済みディレクトリは除外)
-    .pipe(plumber()) // エラーが出てもwatchを止めない
-    .pipe(uglify()) // jsを圧縮
+    .pipe($.plumber()) // エラーが出てもwatchを止めない
+    .pipe($.uglify()) // jsを圧縮
     .pipe(gulp.dest('./js/min/')); // 圧縮後のjsの出力先
 });
 
@@ -51,7 +61,7 @@ gulp.task('js', function() {
  * gulp.watch('watchする対象', ['実行するタスク']);
  * これが基本形。対象が複数存在する場合は配列に格納する。
  */
-gulp.task('default', ['server'], function() {
+gulp.task('default', function() {
   gulp.watch(['js/**/*.js', '!js/min/**/*.js'], ['js']);
   gulp.watch('sass/**/*.scss', ['sass']);
   gulp.watch('sass/**/docco.scss', ['styledocco']);
